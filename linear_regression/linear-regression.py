c5bffa01e317d7f13fdb0fb490bbfd831d76b464
szmgithub <xdsongzhiming@gmail.com>
2016-09-28 15:21:47 +0800

    add ridge_regression and Readme

--- linear_regression/linear-regression.py
+++ linear_regression/linear-regression.py
@@ -4,17 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def warm_up_exercise():
-    """热身练习"""
-    A = None
-    # ====================== 你的代码 ==========================
-    # 在下面加入你的代码，使程序返回一个 5x5 的单位矩阵
-    A = np.ones((5,5))
-
-    # =========================================================
-    return A
-
-
 def plot_data(x, y):
     """绘制给定数据x与y的图像"""
     plt.figure()
@@ -38,11 +27,10 @@ def compute_cost(X, y, theta):
     """计算线性回归的代价。"""
     m = len(y)
     J = 0.0
-    # ====================== 你的代码 ==========================
     # 计算给定 theta 参数下线性回归的代价
-    # 请将正确的代价赋值给 J
+    # 将正确的代价赋值给 J
     for i in range(m):
-        J += (1/(2*m)) * ( ( np.dot(X[i,:], theta) - y[i] ) ** 2)
+        J += (1.0/(2*m)) * ( ( np.dot(X[i,:], theta) - y[i] ) ** 2)
     # =========================================================
     return J
 
@@ -54,9 +42,8 @@ def gradient_descent(X, y, theta, alpha, num_iters):
     theta_history = np.zeros((num_iters,2))
 
     for iter in range(num_iters):
-        # ====================== 你的代码 ==========================
         # 计算给定 theta 参数下线性回归的梯度，实现梯度下降算法
-        theta = theta - alpha*( (1/m) * np.dot( X.T, np.dot(X, theta)-y.reshape(m,1)) )
+        theta = theta - alpha*( (1.0/m) * np.dot( X.T, np.dot(X, theta)-y.reshape(m,1)) )
 #        delt = np.zeros((2,1))
 #        for i in range(m):
 #            delt[0,0] += ( np.dot(X[i, :], theta) - y[i] ) * X[i, 0]
@@ -73,7 +60,6 @@ def gradient_descent(X, y, theta, alpha, num_iters):
 
 def plot_linear_fit(X, y, theta):
     """在绘制数据点的基础上绘制回归得到直线"""
-    # ====================== 你的代码 ==========================
     # 绘制x与y的图像
     # 使用 matplotlib.pyplt 的命令 plot, xlabel, ylabel 等。
     # 提示：可以使用 'rx' 选项使数据点显示为红色的 "x"，
@@ -107,8 +93,7 @@ def plot_visualize_cost(X, y, theta_best):
     J_vals = np.zeros((101, 101))
     for i in range(101):
         for j in range(101):
-            # =============== 你的代码 ===================
-            # 加入代码，计算 J_vals 的值
+            # 计算 J_vals 的值
             t[0,0], t[1,0] = theta0_vals[i], theta1_vals[j]
             J_vals[j, i] = compute_cost(X, y, t)
             # ===========================================
@@ -125,11 +110,6 @@ def plot_visualize_cost(X, y, theta_best):
 
 
 if __name__ == '__main__':
-    print('Running warm-up exercise ... \n')
-    print('5x5 Identity Matrix: \n')
-    A = warm_up_exercise()
-    print(A)
-
     print('Plotting Data ...\n')
     data = np.loadtxt('data.txt', delimiter=',')
     x, y = data[:, 0], data[:, 1]
@@ -156,7 +136,6 @@ if __name__ == '__main__':
     print (J0)
     
     # run gradient descent
-    # Expected value: theta = [-3.630291, 1.166362]
     theta, J_history, theta_history = gradient_descent(X, y, theta,
                                         alpha, iterations)
     print('Theta found by gradient descent:')
